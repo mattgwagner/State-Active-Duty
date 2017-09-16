61f0f81cba089ed510cd5efb89ddfcb3ec9d161c
Matt Wagner <mattgwagner@gmail.com>
2017-09-15 20:23:22 -0400

    Initial code drop

---
+++ StateActiveDuty.Web/Models/PurchaseOrder.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateActiveDuty.Web.Models
+{
+    public class PurchaseOrder
+    {
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        public virtual Unit Unit { get; set; }
+
+        public virtual Vendor Vendor { get; set; }
+
+        public Status OrderStatus { get; set; } = Status.Created;
+
+        // Who submitted it & when
+
+        // Who approved it & when
+
+        public Priority OrderPriority { get; set; } = Priority.Routine;
+
+        public enum Priority : byte
+        {
+            Routine = 0,
+
+            Priority = 1,
+
+            Emergency = 2
+        }
+
+        public enum Status : byte
+        {
+            Created = 0,
+
+            Submitted_to_S4 = 1,
+
+            Sent_to_SQM = 2,
+
+            Requires_Update = 3,
+
+            Approved = 10,
+
+            // Completed / Paid?
+
+            Rejected = byte.MaxValue
+        }
+    }
+
+    public class Vendor
+    {
+        public String Name { get; set; }
+
+        public String PointOfContact { get; set; }
+
+        public String Phone { get; set; }
+
+        public String PointOfContactRole { get; set; }
+
+        public Address PhysicalAddress { get; set; } = new Address { };
+    }
+
+    public class Address
+    {
+        public String Line1 { get; set; }
+
+        public String City { get; set; }
+
+        public String State { get; set; }
+
+        public String ZipCode { get; set; }
+    }
+
+}
---
+++ StateActiveDuty.Web/Models/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateActiveDuty.Web.Models
+{
+    public class Vehicle
+    {
+        public Status VehicleStatus { get; set; } = Status.FMC;
+
+        public virtual Unit Unit { get; set; }
+
+        // Chalk Order?
+
+        // LIN?
+
+        public String Bumper { get; set; }
+
+        public String Nomenclature { get; set; }
+
+        public String Registration { get; set; }
+
+        public String Serial { get; set; }
+
+        // Fuel Card? Towbar? Water Buffalo?
+
+        // Fuel Level?
+
+        public enum Status : byte
+        {
+            Unknown = byte.MinValue,
+
+            FMC = 0,
+
+            NMC = byte.MaxValue
+        }
+    }
+}
